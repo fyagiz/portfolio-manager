1fade30fbd9f3e0a1dd1d67f27b7054beb9c9a94
fyagiz <furkanyagiz@gmail.com>
2023-09-28 07:41:00 +0300

    chore: initialize project

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  extends: ["@react-native", "plugin:react/jsx-runtime", "prettier"],
+  rules: {
+    quotes: [1, "double"],
+    "object-curly-spacing": [2, "always"],
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  arrowParens: "avoid",
+  bracketSameLine: false,
+  singleQuote: false,
+  trailingComma: "all",
+  bracketSpacing: true,
+  printWidth: 250,
+};

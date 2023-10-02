62a9888630b73d8fc8ebd6de89e898e5b1c54b2f
fyagiz <furkanyagiz@gmail.com>
2023-10-02 20:19:40 +0300

    refactor: App.tsx file is refactored to use Dashboard screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
 import { registerRootComponent } from "expo";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import Dashboard from "./screens/Dashboard";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <>
+      <Dashboard />
       <StatusBar style="auto" />
-    </View>
+    </>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default App;
 
 registerRootComponent(App);

4b9984a9317f975d7ae5802f959aeedc7ae13451
fyagiz <furkanyagiz@gmail.com>
2023-10-02 18:35:49 +0300

    test(InvestmentCard): InvestmentCard component tests

---
+++ src/components/InvestmentCard/InvestmentCard.test.tsx
@@ -0,0 +1,46 @@
+import { render, fireEvent } from "@testing-library/react-native";
+import InvestmentCard from "./InvestmentCard";
+
+/*
+1- InvestmentCard should be rendered.
+2- InvestmentCard should show investmentName
+3- InvestmentCard should show profit
+4- InvestmentCard should show profit percentage
+5- InvestmentCard should be pressed
+*/
+
+describe("Card Component Unit Tests", () => {
+  test("should render correctly", () => {
+    const investmentCardComponent = render(<InvestmentCard investmentName="Test Fund" profit="5000 TL" profitPercentage="5" />);
+    expect(investmentCardComponent).toMatchSnapshot();
+  });
+
+  test("should show investmentName", () => {
+    const investmentCardComponent = render(<InvestmentCard investmentName="Test Fund" profit="5000 TL" profitPercentage="5" />);
+    const investmentCardChildren = investmentCardComponent.getByTestId("investmentName").children;
+    const investmantName = investmentCardChildren.join("");
+    expect(investmantName).toMatch("Test Fund");
+  });
+
+  test("should show profit", () => {
+    const investmentCardComponent = render(<InvestmentCard investmentName="Test Fund" profit="5000 TL" profitPercentage="5" />);
+    const investmentCardChildren = investmentCardComponent.getByTestId("profit").children;
+    const profit = investmentCardChildren.join("");
+    expect(profit).toMatch("5000 TL");
+  });
+
+  test("should show profit percentage", () => {
+    const investmentCardComponent = render(<InvestmentCard investmentName="Test Fund" profit="5000 TL" profitPercentage="5" />);
+    const investmentCardChildren = investmentCardComponent.getByTestId("profitPercentage").children;
+    const profitPercentage = investmentCardChildren.join("");
+    expect(profitPercentage).toMatch("% 5");
+  });
+
+  test("should be pressed", () => {
+    const mockFunction = jest.fn();
+    const investmentCardComponent = render(<InvestmentCard investmentName="Test Fund" profit="5000 TL" profitPercentage="5" onPress={mockFunction} testOnly_pressed />);
+    const pressable = investmentCardComponent.getByTestId("pressable");
+    fireEvent(pressable, "click");
+    expect(mockFunction).toBeCalled();
+  });
+});

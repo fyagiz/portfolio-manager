71828ca4c2ac8b0b6ea8bcdda8ca75da3eeb97c3
fyagiz <furkanyagiz@gmail.com>
2023-11-29 01:42:39 +0300

    feat(MainScreen): Add style from outside of the component

--- src/screens/MainScreen/MainScreen.tsx
+++ src/screens/MainScreen/MainScreen.tsx
@@ -1,7 +1,14 @@
+import { View } from "react-native";
 import Dashboard from "../../components/Dashboard";
-
-const MainScreen = () => {
-  return <Dashboard />;
+import styles from "./MainScreen.style";
+import { MainScreenProps } from "./MainScreen.types";
+const MainScreen = (props: MainScreenProps) => {
+  const { style: propStyle } = props;
+  return (
+    <View style={[styles.container, propStyle]}>
+      <Dashboard />
+    </View>
+  );
 };
 
 export default MainScreen;

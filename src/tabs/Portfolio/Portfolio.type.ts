73cb95d9a06717423fa900d301299404e13fa463
fyagiz <furkanyagiz@gmail.com>
2023-12-07 21:15:08 +0300

    chore: Change "ViewStyle" to "StyleProp<ViewStyle>"

--- src/tabs/Portfolio/Portfolio.type.ts
+++ src/tabs/Portfolio/Portfolio.type.ts
@@ -1,7 +1,7 @@
-import { ViewStyle } from "react-native";
+import { StyleProp, ViewStyle } from "react-native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../../utils";
 
 export type PortfolioPropsType = NativeStackScreenProps<typeof RootStackParamList, "Portfolio"> & {
-  style?: ViewStyle;
+  style?: StyleProp<ViewStyle>;
 };

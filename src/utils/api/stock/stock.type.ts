7bcc3099fa0081ec56fa56f10a2f95b8ffd3fbeb
fyagiz <furkanyagiz@gmail.com>
2024-01-26 17:24:44 +0300

    feat(stockType, stockPriceType): Add StockPriceType and Update StockPriceType

---
+++ src/utils/api/stock/stock.type.ts
@@ -0,0 +1,7 @@
+export type BistStockPriceType = {
+  stockCode: number;
+  current: string;
+  dailyChange: string;
+  dailyChangePercentage: string;
+  previousDayClose: string;
+};
--- src/utils/assetTypes/stockType.ts
+++ src/utils/assetTypes/stockType.ts
@@ -1,8 +1,11 @@
+import { BistStockPriceType } from "../api/stock/stock.type";
+
 type StockType = {
   stockCode: string;
   stockName?: string;
   amount?: number;
   totalCost?: number;
+  price?: BistStockPriceType;
 };
 
 export default StockType;

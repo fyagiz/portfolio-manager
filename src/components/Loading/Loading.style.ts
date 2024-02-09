bf475fab8dab9c2b740473ac917474cfd7035d66
fyagiz <furkanyagiz@gmail.com>
2024-02-09 07:53:45 +0300

    feat(Loading): Add background shadow

--- src/components/Loading/Loading.style.ts
+++ src/components/Loading/Loading.style.ts
@@ -1,9 +1,12 @@
 import { StyleSheet } from "react-native";
+import { COLOR, STYLE } from "../../utils/constants";
 
 export default StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: COLOR.loadingBackgroundColor,
+    opacity: STYLE.opacity,
   },
 });

8298b65c61e88c279477d02b45960f2e7da2bc88
fyagiz <furkanyagiz@gmail.com>
2024-02-09 17:26:09 +0300

    feat(constants): Add moment constants

--- src/utils/constants/index.ts
+++ src/utils/constants/index.ts
@@ -1,5 +1,6 @@
 import BOTTOM_TAB from "./BottomTab";
 import COLOR from "./Color";
 import STYLE from "./Style";
+import DATE_FORMAT from "./Moment";
 
-export { BOTTOM_TAB, COLOR, STYLE };
+export { BOTTOM_TAB, COLOR, STYLE, DATE_FORMAT };
